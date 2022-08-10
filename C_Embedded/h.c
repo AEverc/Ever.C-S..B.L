3b34394c675dde42e6a553c85135d34eb2c26ca8
everkiller <96595802+everkiller@users.noreply.github.com>
2022-08-11 01:59:00 +0200

    first commit

---
+++ C_Embedded/h.c
@@ -0,0 +1,13 @@
+/*#include<stdio.h>
+int main(void)
+{
+    printf("Hey U!!!!");
+    return 0;
+}*/
+#include <stdio.h>
+int main(void)
+{
+    int y = 0xFF0B00F1;
+    char x = (char)y;
+    printf("%c", x);
+}
